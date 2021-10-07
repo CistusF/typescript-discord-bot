4020d5c7fd4df513cbf7eb67e3ae864025e067e1
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2021-10-07 11:58:32 +0900

    add Readme

--- clearGitCached.bat
+++ clearGitCached.bat
@@ -1,1 +1,2 @@
+@ECHO OFF
 git rm -r --cached .
--- src/Index.ts
+++ src/Index.ts
@@ -2,7 +2,8 @@ import Client, { intents } from './Class';
 import config from './Config';
 
 const client = new Client({
-    intents: intents
+    intents: intents,
+    partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"]
 });
 
 client.login(config.token);
