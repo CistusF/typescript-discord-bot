51cd11022cbef2ba2d414bef257ac547265b2002
Vendetta <34066247+CistusF@users.noreply.github.com>
2022-06-13 16:03:36 +0000

    OPPS

--- Interfaces/env.ts
+++
@@ -1,7 +0,0 @@
-export type env = {
-    token: string;
-    ownerId: string;
-    prefix: string;
-    debug: boolean;
-    defaultLang: string;
-}
--- Schemas/Users.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const Schema = mongoose.Schema;
-
-export const User = new Schema({
-    lang: String,
-    memo: String,
-    userId: String
-});
